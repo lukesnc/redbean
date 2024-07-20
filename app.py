83736ca0db5e03142f2b9b917c75646dab41cfe4
lukesnc <lukeasimone@gmail.com>
2024-07-20 14:58:26 -0400

    feat!: can play song over phone from search term

--- app.py
+++ app.py
@@ -1,37 +1,71 @@
 #!/usr/bin/env python3
 
-from flask import Flask, Response, url_for
+from flask import Flask, url_for
 from twilio.twiml.voice_response import VoiceResponse
 from yt_dlp import YoutubeDL
-
-YDL_OPTS = {
-    "format": "best",
-    # ℹ️ See help(yt_dlp.postprocessor) for a list of available Postprocessors and their arguments
-    "postprocessors": [
-        {  # Extract audio using ffmpeg
-            "key": "FFmpegExtractAudio",
-            "preferredcodec": "mp3",
-            "preferredquality": "10",  # WORST QUALITY
-        }
-    ],
-}
 
 app = Flask(__name__)
 
 
-# def get_song(search: str) -> str:
-#     # Download with yt-dlp
-#     url = ["https://youtu.be/CzJbz9qSsd0?si=hhPlTJO39mo44gLv"]
-#     with YoutubeDL(YDL_OPTS) as ydl:
-#         error_code = ydl.download(url)
-#
-#     return "test"
+def download_song(search_term: str) -> str | None:
+    # Get first search result from Youtube
+    print(f'searching YouTube for "{search_term}"')
+    ydl_opts = {
+        "default_search": "ytsearch",  # Search on YouTube
+        "quiet": True,  # Suppress logging
+    }
+    with YoutubeDL(ydl_opts) as ydl:
+        search_results = ydl.extract_info(search_term, download=False)
+
+    videos = search_results.get("entries", [])
+    if not videos:
+        print("no search results found")
+        return None
+
+    url = videos[0]["webpage_url"]
+
+    # Download first result
+    ydl_opts = {
+        "format": "best",
+        "restrictfilenames": True,
+        # "paths": {"home": "static"},
+        "outtmpl": "static/song",
+        "postprocessors": [
+            {  # Extract and convert audio using ffmpeg
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "mp3",
+                "preferredquality": "10",  # 10 = worst
+            }
+        ],
+    }
+
+    with YoutubeDL(ydl_opts) as ydl:
+        error_code = ydl.download([url])
+
+    if error_code != 0:
+        return None
+
+    # TODO: the song will always overwrite to this location for now
+    # in the future we could have different filenames and cache them
+    # in the static/ dir
+    return "song.mp3"
 
 
 @app.route("/answer", methods=["GET", "POST"])
 def answer():
+    print("taking incoming call...")
     response = VoiceResponse()
-    response.play(url_for("static", filename="cheerleader.mp3"))
+
+    # Get voice command
+
+    search = "ric flair drip"
+
+    # Fetch desired song
+    filename = download_song(search)
+
+    # Serve song
+    print("playing song...")
+    response.play(url_for("static", filename=filename))
 
     return str(response)
 
